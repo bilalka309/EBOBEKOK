61d369b5158059be13536da6307e0b3ee39e75be
bilal karahan <bilalkarahan309@gmail.com>
2022-09-23 09:35:16 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in=new Scanner(System.in);
+        System.out.println("N1 sayısını giriniz: ");
+        int x= in.nextInt();
+        System.out.println("N2 sayısını giriniz: ");
+        int ebob=1,i=1,y= in.nextInt();
+        while(i<=x){
+            if(x%i==0 && y%i==0){
+                ebob=i;
+            }
+            i++;
+        }
+        System.out.println("EBOB: "+ebob);
+        int z=x*y;
+        while(i<=z){
+            if(i%x==0 && i%y==0){
+                System.out.println("EKOK: "+i);
+                        break;
+            }
+            i++;
+        }
     }
 }
